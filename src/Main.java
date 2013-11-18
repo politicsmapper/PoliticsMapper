17d618a846fa327438659d4c708ae8566dade667
Esben Sorig <esben@sorig.eu>
2013-11-18 16:18:31 +0000

    XML to JSON conversion ready

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import net.sf.json.JSON;
 import net.sf.json.JSONSerializer;
+import net.sf.json.xml.XMLSerializer;
 
 
 public class Main {
 	
 	public static void main(String[] args) {
-		JSONSerializer asd;
+		String xml = "";
+		XMLSerializer xmlSerializer = new XMLSerializer();  
+		JSON json = xmlSerializer.read( xml );  
 		System.out.println("asd");
 	}
 
