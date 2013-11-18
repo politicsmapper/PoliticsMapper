213a328a387cbb73a4d087914d40c8ebfd9547a1
Esben Sorig <esben@sorig.eu>
2013-11-18 16:22:10 +0000

    Added spaces

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,21 @@ import net.sf.json.xml.XMLSerializer;
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		/* Todo: 
+		 * 
+		 * Read xml files from files system
+		 * Convert to json objects
+		 * Find library (jersey?) to send http requests with the json data to elastic search
+		 * 
+		 * Note: elastic search cluster running on politicsmapper.cloudapp.net port: 9200
+		 */
+		
 		String xml = "";
+		
+		
 		XMLSerializer xmlSerializer = new XMLSerializer();  
-		JSON json = xmlSerializer.read( xml );  
-		System.out.println("asd");
+		JSON json = xmlSerializer.read( xml );
 	}
 
 }
