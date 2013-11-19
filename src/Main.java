f7c0b2d2ed471e1b8763e4fea5805b0c3727b050
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2013-11-19 23:45:51 +0000

    xml to string

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import net.sf.json.JSON;
 import net.sf.json.JSONSerializer;
 import net.sf.json.xml.XMLSerializer;
 
 
+@SuppressWarnings("unused")
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		/* Todo: 
 		 * 
@@ -16,11 +22,22 @@ public class Main {
 		 * Note: elastic search cluster running on politicsmapper.cloudapp.net port: 9200
 		 */
 		
-		String xml = "";
+		//String xml = "";
+		//XMLSerializer xmlSerializer = new XMLSerializer();  
+		//JSON json = xmlSerializer.read(xml);
 		
-		
-		XMLSerializer xmlSerializer = new XMLSerializer();  
-		JSON json = xmlSerializer.read( xml );
+		BufferedReader reader = new BufferedReader( new FileReader ("C:/Users/Anuz/Desktop/Query.xml"));
+	    StringBuilder stringBuilder = new StringBuilder();
+	    String line = null;
+	    String lineSeparator = System.getProperty("line.separator");
+	    while((line = reader.readLine())!= null) {
+	        stringBuilder.append(lineSeparator);
+	        stringBuilder.append(line);   
+	    }
+	    reader.close();
+	    String xmlToString = stringBuilder.toString();
+	    System.out.println(xmlToString);
+	    
 	}
 
 }
