9e9222dec79b29cae5414db12ad3dce5f76dc478
anuz935 <anuz.adhikari.12@ucl.ac.uk>
2013-11-20 14:52:24 +0000

    xml to json

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.XML;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import net.sf.json.JSON;
 import net.sf.json.JSONSerializer;
 import net.sf.json.xml.XMLSerializer;
 
 
+@SuppressWarnings("unused")
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		/* Todo: 
 		 * 
@@ -16,11 +26,29 @@ public class Main {
 		 * Note: elastic search cluster running on politicsmapper.cloudapp.net port: 9200
 		 */
 		
-		String xml = "";
+		//String xml = "";
+		//XMLSerializer xmlSerializer = new XMLSerializer();  
+		//JSON json = xmlSerializer.read(xml);
 		
-		
-		XMLSerializer xmlSerializer = new XMLSerializer();  
-		JSON json = xmlSerializer.read( xml );
+		BufferedReader reader = new BufferedReader( new FileReader ("C:/Users/Anuz/Desktop/primary-toc.xml"));
+	    StringBuilder stringBuilder = new StringBuilder();
+	    String line = null;
+	    String lineSeparator = System.getProperty("line.separator");
+	    while((line = reader.readLine())!= null) {
+	        stringBuilder.append(lineSeparator);
+	        stringBuilder.append(line);   
+	    }
+	    reader.close();
+	    String xmlToString = stringBuilder.toString();
+	    System.out.println(xmlToString);
+	    try {
+			JSONObject soapDatainJsonObject = XML.toJSONObject(xmlToString);
+			System.out.println(soapDatainJsonObject);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
 	}
 
 }
