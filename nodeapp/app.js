3cc02bcff313f04369ebbda3a6ba2cb3266e66ac
Esben Sorig <eas@trifork.com>
2014-02-24 17:16:05 +0000

    Add simple similarity

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var search = require('./routes/search');
+var similar = require('./routes/similar');
 var http = require('http');
 var path = require('path');
 
@@ -30,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/search', search.search);
+app.get('/similar/:id', similar.list);
 
 
 http.createServer(app).listen(app.get('port'), function(){
---
+++ nodeapp/routes/similar.js
@@ -0,0 +1,20 @@
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client();
+
+exports.list = function(req, res){
+    var id = req.param("id"); 
+
+    client.mlt({
+      index: 'hansard',
+      type: 'debate',
+      id: id
+    }).then(function (body) {
+      var hits = body.hits.hits;
+      res.render('results', {
+                'hits' : hits
+            });
+    }, function (error) {
+      res.send('Document does not exist');
+    });
+
+};
