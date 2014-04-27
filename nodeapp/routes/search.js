634e025ecd83890c8ee9f8bcb8079023967a4d0e
Esben Sorig <esben@sorig.eu>
2014-04-27 18:06:40 +0100

    Optimised search and similarity results

--- nodeapp/routes/search.js
+++ nodeapp/routes/search.js
@@ -5,7 +5,26 @@ exports.search = function(req, res){
     var query = req.param("q"); 
 
     client.search({
-      q: query
+      index: "hansard",
+      type: "debate",
+      body: {
+        "query": { 
+          "filtered": {
+            "query":  { 
+              "match" : {
+                "content" : query
+              } 
+            },
+            "filter": { 
+              "regexp": {  // Filter out paragraphs that are too short
+                "content": {
+                  "value": ".{20,}"
+                }
+              }
+            }
+          }
+        }
+      }
     }).then(function (body) {
       var hits = body.hits.hits;
       res.render('results', {
--- nodeapp/routes/similar.js
+++ nodeapp/routes/similar.js
@@ -4,19 +4,58 @@ var client = new elasticsearch.Client();
 exports.list = function(req, res){
     var id = req.param("id"); 
 
-    client.mlt({
-      index: 'hansard',
-      type: 'debate',
+    client.getSource({
+      index: "hansard",
+      type: "debate",
       id: id
-    }).then(function (body) {
-      var hits = body.hits.hits;
-      res.render('results', {
-                'hits' : hits,
-                'query' : id,
-                'type' : "Similarity"
-            });
-    }, function (error) {
-      res.send('Document does not exist');
+    }, function (error, response) {
+      console.log(response["content"])
+      console.log(id)
+      client.search({
+        index: 'hansard',
+        type: 'debate',
+        body: {
+          "query": { 
+            "filtered": {
+              "query":  { 
+                "more_like_this_field" : {
+                  "content" : {
+                    "like_text" : response["content"]
+                  }
+                } 
+              },
+              "filter": { 
+                "and": [
+                  {
+                    "regexp": {  // Filter out paragraphs that are too short
+                      "content": {
+                        "value": ".{20,}"
+                      }
+                    }
+                  },
+                  {
+                    "not": {  // Filter out the id we're trying to find similar items for
+                      "ids": {
+                        "type": "debate",
+                        "values":  [id]
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }).then(function (body) {
+        var hits = body.hits.hits;
+        console.log(hits)
+        res.render('results', {
+                  'hits' : hits,
+                  'query' : id,
+                  'type' : "Similarity"
+              });
+      }, function (error) {
+        res.send('Document does not exist');
+      });
     });
-
 };
